afccd6415d91d550c3c6945cd5234bcc37a124c5
YourLocalDuck <prince.reuben@gmail.com>
2024-09-09 13:31:42 -0400

    backend initialized

---
+++ Spring-Backend/textvista/src/main/java/com/tvapp/textvista/TextvistaApplication.java
@@ -0,0 +1,13 @@
+package com.tvapp.textvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextvistaApplication.class, args);
+	}
+
+}
